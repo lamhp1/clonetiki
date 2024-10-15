bd3fcbbb49342345b8956450ee161ba7e8889b8d
lamhp1 <hoanglam10a4hbt@gmail.com>
2024-10-15 10:04:19 +0700

    config-cors-2

--- beclonetiki/src/index.js
+++ beclonetiki/src/index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 3001
 
 app.use(cors(
   {
-    origin: 'https://lamshop-4wt5mpl7u-lams-projects-505f5634.vercel.app/', // Địa chỉ của frontend
+    origin: 'https://lamshop.vercel.app/', // Địa chỉ của frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Cho phép gửi cookie và thông tin xác thực
   }
